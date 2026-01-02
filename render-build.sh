5c93b4534588c5b1bc9aa3ce91ba5b8886c1dde1
playpasscanada-ctrl <playpasscanada@gmail.com>
2026-01-02 20:34:51 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,10 +5,15 @@ set -o errexit
 # Python libraries install karo
 pip install -r requirements.txt
 
-# FFmpeg download aur setup karo
-if [ ! -f ffmpeg-master-latest-linux64-gpl.tar.xz ]; then
-    curl -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
-fi
+# Purani file ho toh hata do (Taaki fresh download ho)
+rm -f ffmpeg-master-latest-linux64-gpl.tar.xz
+
+# FFmpeg download karo (Added -L to follow redirects)
+curl -L -O https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
+
+# Extract karo
 tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz
+
+# Move karo
 mv ffmpeg-master-latest-linux64-gpl/bin/ffmpeg .
 mv ffmpeg-master-latest-linux64-gpl/bin/ffprobe .
