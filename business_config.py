ebe719026d90035808481f541bdb70c41d3b81ce
playpasscanada-ctrl <playpasscanada@gmail.com>
2026-01-10 13:55:28 +0530

    Create business_config.py

---
+++ business_config.py
@@ -0,0 +1,87 @@
+# business_config.py
+
+BUSINESSES = {
+    # --- GTA STYLE (User List) ---
+    "weed_farm": {
+        "name": "Weed Farm 🌿", "price": 100000000, 
+        "income_per_hr": 500000, "max_stock": 5000000, "supply_cost": 50000
+    },
+    "meth_lab": {
+        "name": "Meth Lab 🧪", "price": 110000000, 
+        "income_per_hr": 600000, "max_stock": 6000000, "supply_cost": 60000
+    },
+    "counterfeit_cash": {
+        "name": "Counterfeit Cash 💵", "price": 120000000, 
+        "income_per_hr": 700000, "max_stock": 7000000, "supply_cost": 70000
+    },
+    "cocaine_lockup": {
+        "name": "Cocaine Lockup 🍚", "price": 130000000, 
+        "income_per_hr": 800000, "max_stock": 8000000, "supply_cost": 80000
+    },
+    "bunker": {
+        "name": "Gunrunning Bunker 🔫", "price": 140000000, 
+        "income_per_hr": 900000, "max_stock": 9000000, "supply_cost": 90000
+    },
+    "nightclub": {
+        "name": "Nightclub Empire 🕺", "price": 150000000, 
+        "income_per_hr": 1000000, "max_stock": 10000000, "supply_cost": 100000
+    },
+    "hacking": {
+        "name": "Hacking Facility 💻", "price": 160000000, 
+        "income_per_hr": 1100000, "max_stock": 11000000, "supply_cost": 110000
+    },
+    "casino": {
+        "name": "Underground Casino 🎲", "price": 180000000, 
+        "income_per_hr": 1300000, "max_stock": 13000000, "supply_cost": 130000
+    },
+    "gold": {
+        "name": "Gold Smuggling 🟡", "price": 190000000, 
+        "income_per_hr": 1400000, "max_stock": 14000000, "supply_cost": 140000
+    },
+    "oil": {
+        "name": "Oil Company 🛢️", "price": 200000000, 
+        "income_per_hr": 1500000, "max_stock": 15000000, "supply_cost": 150000
+    },
+
+    # --- NEW BUSINESSES (Added by me) ---
+    "crypto_farm": {
+        "name": "Crypto Server Farm ₿", "price": 210000000, 
+        "income_per_hr": 1600000, "max_stock": 16000000, "supply_cost": 160000
+    },
+    "ai_lab": {
+        "name": "AI Research Lab 🤖", "price": 230000000, 
+        "income_per_hr": 1800000, "max_stock": 18000000, "supply_cost": 180000
+    },
+    "pmc": {
+        "name": "Private Military Army 🎖️", "price": 250000000, 
+        "income_per_hr": 2000000, "max_stock": 20000000, "supply_cost": 200000
+    },
+    "space_station": {
+        "name": "Space Station Resort 🚀", "price": 300000000, 
+        "income_per_hr": 2500000, "max_stock": 25000000, "supply_cost": 250000
+    },
+    "clone_factory": {
+        "name": "Cloning Facility 🧬", "price": 350000000, 
+        "income_per_hr": 3000000, "max_stock": 30000000, "supply_cost": 300000
+    },
+    "fusion_reactor": {
+        "name": "Fusion Energy Plant ⚛️", "price": 400000000, 
+        "income_per_hr": 3500000, "max_stock": 35000000, "supply_cost": 350000
+    },
+    "drone_factory": {
+        "name": "Drone Manufacturing 🛸", "price": 170000000, 
+        "income_per_hr": 1200000, "max_stock": 12000000, "supply_cost": 120000
+    },
+    "luxury_cars": {
+        "name": "Luxury Car Dealership 🏎️", "price": 145000000, 
+        "income_per_hr": 950000, "max_stock": 9500000, "supply_cost": 95000
+    },
+    "cyber_clinic": {
+        "name": "Cybernetics Clinic 🦾", "price": 220000000, 
+        "income_per_hr": 1700000, "max_stock": 17000000, "supply_cost": 170000
+    },
+    "time_machine": {
+        "name": "Time Travel Agency ⏳", "price": 1000000000, 
+        "income_per_hr": 10000000, "max_stock": 100000000, "supply_cost": 1000000
+    }
+}
