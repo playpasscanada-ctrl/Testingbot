fe1936c79c8c1c9def16733a83db06311949f58f
playpasscanada-ctrl <playpasscanada@gmail.com>
2026-01-13 11:28:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ app = Flask(__name__)
 # IDs (Hardcoded as per your portal)
 CLIENT_ID = "1451451135813746700"
 CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
-REDIRECT_URI = "https://testingbot-z0y6.onrender.com/callback"
-WEBSITE_URL = "https://testingbot-z0y6.onrender.com" 
+REDIRECT_URI = "https://testingbot-8pb1.onrender.com/callback"
+WEBSITE_URL = "https://testingbot-8pb1.onrender.com" 
 
 # --- LOAN SYSTEM SETTINGS ---
 MAX_LOAN = 10000000      # 10 Million Limit
@@ -16802,7 +16802,7 @@ from business_config import BUSINESSES # Import Business Settings
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "vikas_bhai_super_secure_key_786")
 CLIENT_ID = os.getenv("DISCORD_CLIENT_ID", "1451451135813746700")
 CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET") 
-REDIRECT_URI = os.getenv("DISCORD_REDIRECT_URI", "https://testingbot-z0y6.onrender.com/callback")
+REDIRECT_URI = os.getenv("DISCORD_REDIRECT_URI", "https://testingbot-8pb1.onrender.com/callback")
 API_ENDPOINT = 'https://discord.com/api/v10'
 LOG_CHANNEL_ID = "1457066104819028089" 
 GUILD_ID = "1257403231127076915"
@@ -18462,7 +18462,7 @@ async def business(interaction: discord.Interaction):
 async def menu_cmd(i: discord.Interaction):
     # 🔗 Configuration (As per your details)
     CLIENT_ID = "1451451135813746700" 
-    REDIRECT_URI = "https://testingbot-z0y6.onrender.com/callback"
+    REDIRECT_URI = "https://testingbot-8pb1.onrender.com/callback"
     
     # URL Encoding
     import urllib.parse
